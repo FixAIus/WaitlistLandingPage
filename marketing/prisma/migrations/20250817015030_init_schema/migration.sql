4e546c03fccdcb8c888f2e1a0c577dd92e94b6eb
mariobd38 <mariobd38@gmail.com>
2025-08-16 18:50:51 -0700

    redo schema migration

--- marketing/prisma/migrations/20250817015030_init_schema/migration.sql
+++ marketing/prisma/migrations/20250817015030_init_schema/migration.sql
@@ -11,9 +11,25 @@ CREATE TABLE "public"."Waitlist" (
     "planned_usage" TEXT,
     "business_instagram" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "joinedAt" TIMESTAMP(3),
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Waitlist_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Clients" (
+    "id" TEXT NOT NULL,
+    "clerk_id" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "email" TEXT NOT NULL,
+    "first_name" TEXT,
+    "full_name" TEXT,
+    "last_name" TEXT,
+    "profile_image_url" TEXT,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Clients_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -21,3 +37,9 @@ CREATE UNIQUE INDEX "Waitlist_clerk_id_key" ON "public"."Waitlist"("clerk_id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Waitlist_email_key" ON "public"."Waitlist"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Clients_clerk_id_key" ON "public"."Clients"("clerk_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Clients_email_key" ON "public"."Clients"("email");
